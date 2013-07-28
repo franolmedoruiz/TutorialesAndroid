80062564cd359bae0cc870f55cbb2f02e68df604
franolmedoruiz <franolmedoruiz@gmail.com>
2013-07-28 22:47:53 +0200

    Project Completed.

--- Tutorial01/Tutorial01/src/main/java/net/tutoriales/tutorial01/MainActivity.java
+++ Tutorial01/Tutorial01/src/main/java/net/tutoriales/tutorial01/MainActivity.java
@@ -1,10 +1,16 @@
 package net.tutoriales.tutorial01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    public final static String USERNAME = "Unknown";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +18,20 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    /** Called when the user clicks the Send button */
+    public void Send(View view) {
+        Intent intent = new Intent(this, MessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.txtUserName);
+        String message = editText.getText().toString();
+        intent.putExtra(USERNAME, message);
+
+        startActivity(intent);
+    }
 }
